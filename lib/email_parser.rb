a85767772688b73361b6fcc8d5fdae81bbd1b1f6
Broderick  Fowler Jr <broderickfowler@outlook.com>
2019-08-24 23:12:08 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,17 +3,19 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 
-class EmailAddressParser
-  
+class EmailParser
+
   attr_accessor :emails
-  
+
   def initialize(emails)
     @emails = emails
   end
-  
+
   def parse
     email_array = []
-
+    email_array = @emails.split(/[,\s]/)
+    email_array = email_array.reject { |e| e == ""}
+    email_array.uniq
   end
-  
+
 end
